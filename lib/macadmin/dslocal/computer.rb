34e13d01099075baec70c174f98b14f236d746da
Brian Warsing <bcw@sfu.ca>
2013-09-05 08:34:04 -0700

    add defaults method

--- lib/macadmin/dslocal/computer.rb
+++ lib/macadmin/dslocal/computer.rb
@@ -1,6 +1,17 @@
 module MacAdmin
   
   class Computer < DSLocalRecord
+    
+    # Handle required but unspecified record attributes
+    # - generates missing attributes
+    # - changes are merged into the composite record
+    def defaults(data)
+      defaults = {
+        'realname' => ["#{data['name'].first}"],
+      }
+      super defaults.merge(data)
+    end
+    
   end
   
 end
