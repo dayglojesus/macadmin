eb293a9b3c9873240392b3862b63e865634e4ec7
Brian Warsing <bcw@sfu.ca>
2013-09-19 15:33:17 -0700

    new spec for MCX module

---
+++ spec/mcx_spec.rb
@@ -0,0 +1,122 @@
+require 'spec_helper'
+require 'fileutils'
+
+describe MacAdmin::MCX do
+  
+  before :all do
+    # Create a dslocal sandbox
+    @test_dir = "/private/tmp/macadmin_computer_test.#{rand(100000)}"
+    MacAdmin::DSLocalRecord.send(:remove_const, :DSLOCAL_ROOT)
+    MacAdmin::DSLocalRecord::DSLOCAL_ROOT = File.expand_path @test_dir
+    FileUtils.mkdir_p "#{@test_dir}/Default/computers"
+    
+    # Create two computer record plists that we can load
+    planet_express = { 
+      "en_address"    => ["aa:aa:aa:aa:aa:aa"],
+      "realname"      => ["Planet Express"],
+      "name"          => ["planet-express"],
+      "generateduid"  => ["00000000-0000-0000-0000-000000000001"],
+    }
+    
+    [planet_express].each do |computer|
+      record = CFPropertyList::List.new
+      record.value = CFPropertyList.guess(computer)
+      record.save("#{@test_dir}/Default/computers/#{computer['name'].first}.plist", CFPropertyList::List::FORMAT_BINARY)
+    end
+    
+    @policy_as_file = "#{@test_dir}/policy.plist"
+    @raw_xml_content = <<-RAW_XML_CONTENT
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+<key>com.apple.SoftwareUpdate</key>
+<dict>
+	<key>CatalogURL</key>
+	<dict>
+		<key>state</key>
+		<string>always</string>
+		<key>value</key>
+		<string>http://foo.bar.com/reposado/html/content/catalogs/index.sucatalog</string>
+	</dict>
+</dict>
+<key>com.apple.screensaver</key>
+<dict>
+	<key>askForPassword</key>
+	<dict>
+		<key>state</key>
+		<string>once</string>
+		<key>value</key>
+		<integer>1</integer>
+	</dict>
+</dict>
+</dict>
+</plist>
+    RAW_XML_CONTENT
+    
+    File.open(@policy_as_file, 'w') { |f| f.write(@raw_xml_content) }
+    
+  end
+  
+  describe "#mcx_settings=" do
+    
+    context "when the content is XML (String)" do
+      subject { Computer.new :name => 'planet-express' }
+      it { subject.send(:mcx_settings=, @raw_xml_content).should be_an_instance_of Array }
+    end
+    
+    context "when the content is a file path (String)" do
+      subject { Computer.new :name => 'planet-express' }
+      it { subject.send(:mcx_settings=, @policy_as_file).should be_an_instance_of Array }
+    end
+    
+  end
+  
+  describe "#mcx_settings" do
+    subject { Computer.new :name => 'planet-express' }
+    it do
+      subject.send(:mcx_settings=, @raw_xml_content)
+      subject.mcx_settings.should be_an_instance_of Array
+    end
+  end
+  
+  describe '#has_mcx?' do
+    
+    context "when the record has a valid mcx_settings attribute" do
+      subject { Computer.new :name => 'planet-express' }
+      it do
+        subject.send(:mcx_settings=, @raw_xml_content)
+        subject.send(:has_mcx?).should be_true
+      end
+    end
+
+    context "when the record has an empty mcx_settings attribute" do
+      subject { Computer.new :name => 'planet-express' }
+      it do
+        subject['mcx_settings'] = []
+        subject.send(:has_mcx?).should be_false
+      end
+    end
+    
+    context "when the record has a bad mcx_settings attribute" do
+      subject { Computer.new :name => 'planet-express' }
+      it do
+        subject['mcx_settings'] = ''
+        subject.send(:has_mcx?).should be_false
+      end
+    end
+
+    context "when the record has NO mcx_settings attribute" do
+      subject { Computer.new :name => 'planet-express' }
+      it do
+        subject.send(:has_mcx?).should be_false
+      end
+    end
+    
+  end
+  
+  after :all do
+    FileUtils.rm_rf @test_dir
+  end
+  
+end
