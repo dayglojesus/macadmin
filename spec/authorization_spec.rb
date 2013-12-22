89171790f9d3b976edc071e376b88b4c83c8a792
Brian Warsing <dayglojesus@gmail.com>
2013-12-22 13:23:40 -0800

    add specs for the Authorization methods in authorization.c

--- spec/authorization_spec.rb
+++ spec/authorization_spec.rb
@@ -2,5 +2,23 @@ require 'spec_helper'
 require 'macadmin/common'
 
 describe MacAdmin::Authorization do
-
+  
+  describe '#get_authorization_right' do
+    it "should return an Hash object" do
+      MacAdmin::Authorization.get_authorization_right("system.preferences").should be_an_instance_of Hash
+    end
+  end
+  
+  describe '#set_authorization_right' do
+    it "should return an Fixnum object" do
+      MacAdmin::Authorization.set_authorization_right("foo.bar", {}).should be_an_instance_of Fixnum
+    end
+  end
+  
+  describe '#rm_authorization_right' do
+    it "should return an Fixnum object" do
+      MacAdmin::Authorization.rm_authorization_right("foo.bar").should be_an_instance_of Fixnum
+    end
+  end
+  
 end
