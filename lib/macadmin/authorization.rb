14f6254ed06d03b98841c61f17e472fdaf2b1e72
Brian Warsing <dayglojesus@gmail.com>
2014-01-03 13:10:18 -0800

    - implement AuthorizationRight#create - fix #exists? - add AuthorizationStatus class for defining status codes returned from CF funcs

--- lib/macadmin/authorization.rb
+++ lib/macadmin/authorization.rb
@@ -3,6 +3,37 @@ module MacAdmin
   module Authorization
   end
   
+  # AuthorizationStatus
+  # - object comprising a code and description pertaining to the exit status of the database operation
+  # - see Result Codes: https://developer.apple.com/library/mac/documentation/security/Reference/authorization_ref
+  class AuthorizationStatus
+    
+    RESULT_CODES = {
+            0 => "The operation completed successfully.",
+       -60001 => "The set parameter is invalid.",
+       -60002 => "The authorization parameter is invalid.",
+       -60003 => "The tag parameter is invalid.",
+       -60004 => "The authorizedRights parameter is invalid.",
+       -60005 => "The Security Server denied authorization for one or more requested rights. This error is also returned if there was no definition found in the policy database, or a definition could not be created.",
+       -60006 => "The user canceled the operation.",
+       -60007 => "The Security Server denied authorization because no user interaction is allowed.",
+       -60008 => "An unrecognized internal error occurred.",
+       -60009 => "The Security Server denied externalization of the authorization reference.",
+       -60010 => "The Security Server denied internalization of the authorization reference.",
+       -60011 => "The flags parameter is invalid.",
+       -60031 => "The tool failed to execute.",
+       -60032 => "The attempt to execute the tool failed to return a success or an error code.",
+    }
+    
+    attr_reader :code, :description
+    
+    def initialize(code)
+      @code = code
+      @description = RESULT_CODES[@code]
+    end
+    
+  end
+  
   # AuthorizationRight
   # - creates and manages OS X Authorization database rithgs
   # - params: :name, :definition
@@ -16,13 +47,13 @@ module MacAdmin
     # This file contains the defaults for all the rights & rules
     AUTHORIZATION_DB_DEFAULTS = '/System/Library/Security/authorization.plist'
     
-    attr_reader :name, :definition
+    attr_reader :name, :definition, :status
     
     def initialize(name, definition = {})
       validate(name, definition)
       @name = name
-      @real = Authorization.get_authorization_right(@name)
-      @definition = @real.merge(normalize(definition))
+      @real = Authorization.get_authorization_right @name
+      @definition = @real.merge normalize(definition)
       self
     end
     
@@ -32,10 +63,22 @@ module MacAdmin
       @definition = normalize(hash)
     end
     
+    # Create the right in the OS X Authorization database
+    # - returns true if successful, false otherwise
+    # - sets @status using AuthorizationStatus object
+    def create
+      result  = Authorization.set_authorization_right @name, @definition
+      @status = AuthorizationStatus.new result
+      return false unless result == 0
+      @definition = Authorization.get_authorization_right @name
+      true
+    end
+    
     # Does the specified resource already exist?
     # - returns Boolean
     def exists?
-      @real = Authorization.get_authorization_right(@name)
+      @real = Authorization.get_authorization_right @name
+      @definition = @real.merge @definition
       @definition.eql? @real
     end
     
